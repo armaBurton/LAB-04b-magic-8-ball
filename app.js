0ee2372b637c72e7f42f6b4a0817bf745778b6a4
armaBurton <armanarma@armaburton.com>
2021-10-08 12:51:15 -0700

    inital commit, created 8-ball shape, added keyframes for 'shake' & 'opacity'

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // import functions
 
 // reference needed DOM elements
-
+const roll = document.getElementById('outter-shape');
+const cube = document.getElementById('cube');
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+roll.addEventListener('click', () => {
+    roll.style.animation = "shake 1s";
+    setTimeout(() => {
+        roll.
+    })
+});
