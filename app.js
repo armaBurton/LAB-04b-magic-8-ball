17fa658f420ce5c4b12f104070324dca819dbb21
armaBurton <armanarma@armaburton.com>
2021-10-10 10:10:03 -0700

    added random message generator, added animation to message cube, styled 8-ball, styled body/header/footer/

--- app.js
+++ app.js
@@ -3,12 +3,54 @@
 // reference needed DOM elements
 const roll = document.getElementById('outter-shape');
 const cube = document.getElementById('cube');
+const fortune = document.getElementById('fortune');
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+function randomizeMessage() {
+    const messageArray = [
+        `It is certain.`, 
+        `It is decidedly so.`,
+        `Without a doubt.`,
+        `Yes definitely.`,
+        `You may rely on it`,
+        `As I see it, yes.`,
+        `Most likely.`,
+        `Outlook good.`,
+        `Yes.`,
+        `Signs point to yes.`,
+        `Reply hazy, try again.`,
+        `Ask again later.`,
+        `Better not tell you now.`,
+        `Cannot predict now.`,
+        `Concentrate and ask again.`,
+        `Don't count on it.`,
+        `My reply is no.`,
+        `My sources say no.`,
+        `Outlook not so good.`,
+        `Very doubtful.`
+    ];
+
+    const randoNumber = Math.floor(Math.random() * 21);
+
+    return(messageArray[randoNumber]);
+}
+
 roll.addEventListener('click', () => {
     console.log('clicked');
-    //roll.classList.add('shake8ball');
+    roll.classList.add('shake8ball');
+    setTimeout(() => {
+        roll.classList.remove('shake8ball');
+        const message = randomizeMessage();
+        fortune.textContent = message;
+        cube.classList.add('cubeVisibility');
+        setTimeout(() => {
+            cube.classList.remove('cubeVisibility');
+        }, 3001);
+    }, 1001);
+    
 });
+
